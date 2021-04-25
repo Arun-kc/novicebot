f545912ebc0c89778e5436c50720032f22e0ca0b
Arun-kc <arunkc91@gmail.com>
2021-04-25 15:15:13 +0530

    added githubstats command

---
+++ commands/utility/githubstats.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+
+async function getJson (response){
+    let json = await response.json()
+    return json
+}
+
+module.exports = {
+    name: 'githubstats',
+    async execute(msg, args){
+        let user = args[0]
+        let url =  `https://api.github.com/users/${user}`
+        let response = await fetch(url);
+        let json = await getJson(response)
+        console.log(json);
+
+        let responseStaredRepos = await fetch(`https://api.github.com/users/${json.login}/starred`);
+        let noOfStaredRepos = await getJson(responseStaredRepos)
+
+        let responseTotalRepos = await fetch(`https://api.github.com/users/${json.login}/repos`);
+        let noOfTotalRepos = await getJson(responseTotalRepos)
+        
+        const githubEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(`Github stats of ${json.login}`)
+        .setURL(json.html_url)
+        .setThumbnail(json.avatar_url)
+        .addFields(
+            { name: `Stared Repositories : ${noOfStaredRepos.length}`,value: '\u200B'},
+            { name: `Total Repositores : ${noOfTotalRepos.length}`,value: '\u200B'},
+        )
+        .setTimestamp();
+
+    msg.channel.send(githubEmbed);
+
+    }
+    
+};
