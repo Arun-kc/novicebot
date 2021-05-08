f5767f4dd4ba17705578ff4114588d964fc616cc
Arun-kc <arunkc91@gmail.com>
2021-05-08 17:40:12 +0530

    Added avatar command

---
+++ src/commands/utility/avatar.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'avatar',
+    description: 'Returns a users avatar',
+    async execute(msg, args){
+        
+        const embed = new Discord.MessageEmbed()
+        
+        if(!msg.mentions.users.first()){
+            embed.setTitle('Your Avatar:')
+            embed.setDescription('This is your avatar')
+            embed.setImage(msg.author.displayAvatarURL())
+            embed.setColor('RANDOM')
+        }else{
+            const user = msg.mentions.users.first()
+            embed.setTitle(`${user.tag}'s Avatar:`)
+            embed.setImage(user.displayAvatarURL())
+            embed.setColor('RANDOM')
+        }
+
+        msg.channel.send(embed);
+
+    }
+    
+};
