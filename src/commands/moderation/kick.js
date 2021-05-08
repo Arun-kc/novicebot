88f0a54f8a33daf65829d062a5f535799b4d7e9b
Arun-kc <arunkc91@gmail.com>
2021-05-08 20:47:33 +0530

    Added kick command

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 
 module.exports = {
-	name: 'ban',
-	description: 'Bans a user',
+	name: 'kick',
+	description: 'Kicks a user',
 	// eslint-disable-next-line no-unused-vars
 	async execute(msg, args) {
 
@@ -11,7 +11,7 @@ module.exports = {
 		const tag = `<@${member.id}>`;
 
 		if(member.hasPermission('ADMINISTRATOR') ||
-            member.hasPermission('BAN_MEMBERS')
+            member.hasPermission('KICK_MEMBERS')
 		) {
 
 			const target = mentions.users.first();
@@ -19,22 +19,19 @@ module.exports = {
 			if(target) {
 				const targetMember = msg.guild.members.cache.get(target.id);
 				targetMember.ban();
-				// msg.channel.send(`${tag} User ${target.tag} has been banned`);
 				const embed = new Discord.MessageEmbed()
 					.setColor('RED')
-					.setDescription(`✅ ${tag} User ${target.tag} has been banned successfully!!`);
+					.setDescription(`✅ ${tag} User ${target.tag} has been kicked successfully!!`);
 				msg.channel.send(embed);
 			}
 			else {
-				// msg.channel.send(`${tag} Please specify someone to ban`);
 				const embed = new Discord.MessageEmbed()
 					.setColor('RED')
-					.setDescription(`❌ ${tag} Please specify someone to ban!!`);
+					.setDescription(`❌ ${tag} Please specify someone to kick!!`);
 				msg.channel.send(embed);
 			}
 		}
 		else {
-			// msg.channel.send(`${tag} You do not have permission to use this commmand`);
 			const embed = new Discord.MessageEmbed()
 				.setColor('RED')
 				.setDescription(`❌ ${tag} You do not have permission to use this commmand!!`);
