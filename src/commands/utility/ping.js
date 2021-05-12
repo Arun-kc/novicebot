9e69cd26ff58a536dd7152c88ba7747eabe07263
Arun-kc <arunkc91@gmail.com>
2021-05-12 20:13:50 +0530

    Added ping command

---
+++ src/commands/utility/ping.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'ping',
+	description: '',
+	// eslint-disable-next-line no-unused-vars
+	execute(msg, args) {
+		msg.reply('🏓 Calculating ping.....').then((resultMsg) => {
+			const ping = resultMsg.createdTimestamp - msg.createdTimestamp;
+			resultMsg.edit(`🏓 **Bot Latency: ${ping}, API Latency: ${msg.client.ws.ping}** 🏓`);
+		});
+	},
+};
