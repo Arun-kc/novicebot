5d10febd64b627aac01efac3fd8e3d0d602c73ae
Arun-kc <arunkc91@gmail.com>
2021-05-14 17:37:16 +0530

    Updated hi command with replies

--- src/commands/fun/hi.js
+++ src/commands/fun/hi.js
@@ -1,8 +1,46 @@
 const replies = [
 	'Ahoy!!',
 	'Aloha!!',
+	'Hola',
+	'Que pasa',
+	'Bonjour',
+	'Hallo',
+	'Ciao',
+	'Konnichiwa',
 	'yare yare',
 	'do something else bud!',
+	'Hello, sunshine!',
+	'Howdy, partner!',
+	'Hey, howdy, hi!',
+	'What’s kickin’, little chicken?',
+	'Peek-a-boo!',
+	'Howdy-doody!',
+	'Hey there, freshman!',
+	'My name\'s Zoro, and I\'m the world\'s greatest swordsman!',
+	'Hi, mister!',
+	'I come in peace!',
+	'Put that cookie down!',
+	'Ahoy, matey!',
+	'Hiya!',
+	'‘Ello, gov\'nor!',
+	'Top of the mornin’ to ya!',
+	'What’s crackin’?',
+	'GOOOOOD MORNING, EARTHLING!',
+	'‘Sup, homeslice?',
+	'This call may be recorded for training purposes',
+	'Howdy, howdy ,howdy!',
+	'How does a lion greet the other animals in the field? A: Pleased to eat you',
+	'Hello, my name is Roronoa Zoro',
+	'I\'m Batman',
+	'At least, we meet for the first time for the last time!',
+	'Hello, who\'s there, I\'m talking',
+	'Here\'s Johnny!',
+	'You know who this is',
+	'Ghostbusters, whatya want?',
+	'Yo!',
+	'Whaddup',
+	'Greetings and salutations!',
+	'Doctor!!',
 ];
 
 module.exports = {
@@ -14,7 +52,7 @@ module.exports = {
 	execute(client, msg, args) {
 		const index = Math.floor(Math.random() * replies.length);
 		// msg.reply(replies[index]);
-		msg.channel.send(`${replies[index]} <@${msg.author.id}>`);
+		msg.channel.send(`**${replies[index]}** <@${msg.author.id}>`);
 
 	},
 };
