cae131ffbd5e57ce3de1d21b2a536b6dcd608bda
Arun-kc <arunkc91@gmail.com>
2021-05-14 19:27:54 +0530

    Added choose command

---
+++ src/commands/fun/choose.js
@@ -0,0 +1,20 @@
+module.exports = {
+	name: 'choose',
+	category: 'fun',
+	aliases : ['ch'],
+	usage: '<choice1> | <choice2> | ...',
+	description: 'Random choose between choices',
+	// eslint-disable-next-line no-unused-vars
+	execute(client, msg, args) {
+		const replies = args.join('').split('|');
+		const index = Math.floor(Math.random() * replies.length);
+		// msg.reply(replies[index]);
+		if(args.length < 2) {
+			msg.channel.send('**Please provide more than 1 choice**');
+		}
+		else{
+			msg.channel.send(`☑️ **${msg.member.nickname}**, My choice is : **${replies[index]}**`);
+		}
+
+	},
+};
