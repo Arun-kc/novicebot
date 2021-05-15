044aea4e50e5f84e2893ff431087f91199eb180d
Arun-kc <arunkc91@gmail.com>
2021-05-15 12:56:27 +0530

    Added info command

---
+++ src/commands/utility/info.js
@@ -0,0 +1,85 @@
+const Discord = require('discord.js');
+const { version } = require('../../../package.json');
+
+module.exports = {
+	name: 'info',
+	aliases : ['botinfo'],
+	category: 'utility',
+	description: 'Returns the bots informations',
+	// eslint-disable-next-line no-unused-vars
+	async execute(client, msg, args) {
+
+		const { users, guilds, channels, uptime } = client;
+
+		const embed = new Discord.MessageEmbed()
+			.setAuthor(`⚔️ ${client.user.username} ⚔️`, client.user.displayAvatarURL())
+			.setColor('#1f782f')
+			.setThumbnail(`https://cdn.discordapp.com/avatars/${client.user.id}/${client.user.avatar}.png`)
+			.addFields(
+				{
+					name: 'Creator',
+					value: 'blacksheep#1200',
+					inline: true,
+				},
+				{
+					name: 'Version',
+					value: version,
+					inline: true,
+				},
+				{
+					name: 'Servers',
+					value: guilds.cache.size,
+					inline: true,
+				},
+				{
+					name: 'Users',
+					value: users.cache.size,
+					inline: true,
+				},
+				{
+					name: 'Channels',
+					value: channels.cache.size,
+					inline: true,
+				},
+			)
+			.addFields(
+				{
+					name: 'Uptime',
+					value: uptime,
+					inline: true,
+				},
+				{
+					name: 'Library',
+					value: 'Discord.js',
+					inline: true,
+				},
+			)
+			.addFields(
+				{
+					name: 'Website',
+					value: 'coming soon...',
+				},
+				{
+					name: 'Support Server',
+					value: 'coming soon...',
+				},
+				{
+					name: 'Invite Link',
+					value: 'coming soon...',
+				},
+				{
+					name: 'Github Link',
+					value: 'coming soon...',
+				},
+				{
+					name: 'Sponsor',
+					value: 'coming soon...',
+				},
+			)
+			.setTimestamp();
+
+		msg.channel.send(embed);
+
+	},
+
+};
