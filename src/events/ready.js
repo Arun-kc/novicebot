2d9ab735a767f41571784cdd748959ee46efa63b
Arun-kc <arunkc91@gmail.com>
2021-05-21 21:11:07 +0530

    Updated bot presence status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,3 +1,8 @@
-module.exports = () => {
+// eslint-disable-next-line no-unused-vars
+module.exports = (Discord, client, message) => {
 	console.log('working bud!!');
+	console.log(client.user);
+	client.user.setPresence({ activity: { name: 'z!help' }, status: 'online' })
+		.then(console.log)
+		.catch(console.error);
 };
