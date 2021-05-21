986522b6846fa78c4f3ea508af03adec039d7db7
Arun-kc <arunkc91@gmail.com>
2021-05-21 21:35:02 +0530

    Added heroku procfile

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = (Discord, client, message) => {
 	console.log('working bud!!');
-	console.log(client.user);
 	client.user.setPresence({ activity: { name: 'z!help' }, status: 'online' })
 		.then(console.log)
 		.catch(console.error);
